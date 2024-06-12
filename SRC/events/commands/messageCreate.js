5e127c774f82c71524addb768504348cca9b5c7f
perosofic420 <115893984+perosofic420@users.noreply.github.com>
2024-06-12 21:09:52 +0100

    data folder stuff + prepare msg create for lvl system

--- SRC/events/commands/messageCreate.js
+++ SRC/events/commands/messageCreate.js
@@ -2,22 +2,7 @@ module.exports = {
     name: 'messageCreate',
     once: false,
     async execute(message, client) {
-        if (!message.content.startsWith("/") || message.author.bot) { return; }
-        if (message.author.id != 884532532857683990 && message.author.id != 856639315743604807) { return; }
-
-        var commandName = message.content.split(' ')[0].split('/')[1];
-        var args = message.content.split(' ').slice(1);
-
-        const { adminCommands } = client;
-
-        const command = adminCommands.get(commandName);
-        if (!command) return;
-
-        try {
-            await command.execute(message, client, args);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({content: `An issue occurred executing this command.`, ephemeral: true });
-        }
+        if (message.author.bot) { return; }
+        
     }
 }
